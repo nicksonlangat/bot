9b3af105cbaf512f134a715e454a3ef5baa6f302
LoyalFlames <nicksonlangat95@gmail.com>
2020-06-20 15:22:30 +0300

    f

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 #from twilio.twiml.messaging_response import MessagingResponse
- from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 # import requests
 import datetime
